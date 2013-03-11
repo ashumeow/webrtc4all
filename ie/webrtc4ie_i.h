a931a148826a4f4a0ae9f712283d078e46776e80
bossiel@yahoo.fr <bossiel@yahoo.fr@5cdbfa38-78f0-225e-2531-33f739bda9b1>
2013-03-11 08:46:03 +0000

    Align with Doubango 833

--- ie/webrtc4ie_i.h
+++ ie/webrtc4ie_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0500 */
-/* at Thu Sep 13 21:58:01 2012
+/* at Sun Mar 10 05:02:29 2013
  */
 /* Compiler settings for .\webrtc4ie.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
