079c6468c93ce33fb579ab53a00ed20ba7a7f03a
bossiel@yahoo.fr <bossiel@yahoo.fr@5cdbfa38-78f0-225e-2531-33f739bda9b1>
2013-05-17 10:12:42 +0000

    Gotham City - Initial commit

---
+++ trunk/gotham/MFT_WebRTC4All/w4all.h
@@ -0,0 +1,21 @@
+/*
+* Copyright (C) 2013 Gotham City. All rights reserved.
+*/
+/**@file w4all.h
+ * @brief Main header file
+ *
+ * @author Batman@GothamCity
+ */
+#pragma once
+
+#include <mfapi.h>
+#include <mftransform.h>
+#include <mfidl.h>
+#include <mferror.h>
+
+#include <shlwapi.h>
+
+#include <strsafe.h>
+
+void DllAddRef();
+void DllRelease();
---
+++ trunk/gotham/test/stdafx.h
@@ -0,0 +1,19 @@
+/*
+* Copyright (C) 2013 Gotham City. All rights reserved.
+*/
+/**@file dllmain.cc
+ * @brief Implementation for DLL exports and COM class factory for registration. 
+ * If 'Regsvr32.exe' works it's maily because of this.
+ *
+ * @author Batman@GothamCity
+ */
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
